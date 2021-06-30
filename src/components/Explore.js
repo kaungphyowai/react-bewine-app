49e398a6264ea3ac2981570db9974ffb774bde28
Kaung Phyo Wai <kaungphyowaidecember@gmail.com>
2021-06-30 23:39:57 +0630

    first two section styling and responsive(mobile and desktop) complete

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,60 +1,89 @@
-import { Button ,makeStyles, Grid, Typography } from '@material-ui/core'
-import React from 'react'
-
-const useStyles = makeStyles(theme => (
-    {
-        root: {
-            minHeight: "100vh",
-            backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg1.jpg"})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "50% 50%",
-            [theme.breakpoints.up("sm")]:{
-              minHeight: "100",
-              backgroundSize: "cover",
-              backgroundPosition: "50% 50%",
-              position: "relative",
-              width: "100%",
-              height: "100%",
-            }
-        },
-    }
-))
+import { Button, makeStyles, Grid, Typography, Box } from "@material-ui/core";
+import React from "react";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "70vh",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/bg1.jpg"})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "50% 50%",
+    [theme.breakpoints.up("sm")]: {
+      minHeight: "100vh",
+      backgroundSize: "cover",
+      backgroundPosition: "50% 50%",
+      position: "relative",
+      width: "100%",
+      height: "100%",
+    },
+  },
+  titleStyle: {
+    fontFamily: "Josefin Sans",
+    fontWeight: "bolder",
+    textAlign: "center",
+    fontSize: "250%",
+    color: "#fff",
+    lineHeight: "100%",
+    [theme.breakpoints.up("sm")]: {
+      textAlign: "start",
+      fontSize: "450%",
+      fontWeight: "bolder",
+    },
+  },
+  buttonStyle: {
+    color: "#fff",
+    fontFamily: "Josefin Sans",
+    padding: "10px 15px",
+    [theme.breakpoints.up("sm")]: {
+      padding: "15px 30px",
+    },
+  },
+}));
 const Explore = () => {
-    const classes = useStyles();
-    return (
-       
-        <Grid container className={classes.root} alignContent="center" >
-          {/* first padding */}
-          <Grid item xs={2} sm={2} />
-          {/* Content */}
-          <Grid  item container xs={8} sm={4} spacing={5}>
-            {/* Text */}
-            <Grid item container xs={12} sm={12}>
-            
-            <Typography  color="secondary" variant="h3">
-                    Explore our wide variety of wine
+  const classes = useStyles();
+  return (
+    <Box pt={4} style={{ backgroundColor: "#064125", minWidth: "100%" }}>
+      <Grid container className={classes.root} alignContent="center">
+        {/* first padding */}
+        <Grid item xs={3} sm={2} />
+        {/* Content */}
+        <Grid item container xs={8} sm={4} spacing={5}>
+          {/* Text */}
+          <Grid item container xs={12} sm={12}>
+            <Typography variant="h3" className={classes.titleStyle}>
+              EXPLORE OUR WIDE VARIETY OF WINES
             </Typography>
-            </Grid>
-            {/* Button */}
-            <Grid  item container xs ={12} sm={12}>
-              <Grid item container xs={12} sm={7}>
-                <Grid item container xs={6} sm={6}>
-                <Button variant="contained" >Book A Tour</Button>
-                  </Grid>
-                  <Grid item container xs={6} sm={6}>
-                <Button variant="contained">Our Wines</Button> 
-                  </Grid>
-              </Grid>
-              {/* Button left padding */}
-              <Grid item xs={2} sm={5} />
-            </Grid>
           </Grid>
-          {/* last padding */}
-          <Grid item xs={2} sm={6} />
+          {/* Button */}
+          <Grid item container xs={12} sm={12} justify="center">
+            <Grid item container xs={12} sm={8}>
+              <Grid item container xs={6} sm={6}>
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: "#62021D" }}
+                  className={classes.buttonStyle}
+                >
+                  Book A Tour
+                </Button>
+              </Grid>
+              <Grid item container xs={6} sm={6}>
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: "#80A094" }}
+                  className={classes.buttonStyle}
+                >
+                  Our Wines
+                </Button>
+              </Grid>
+            </Grid>
+            {/* Button left padding */}
+            <Grid item xs={2} sm={4} />
+          </Grid>
         </Grid>
-       
-    )
-}
+        {/* last padding */}
+        <Grid item xs={1} sm={6} />
+      </Grid>
+    </Box>
+  );
+};
 
-export default Explore
+export default Explore;
--- src/components/QuickContact.js
+++ src/components/QuickContact.js
@@ -1,38 +1,95 @@
-import { Box, Grid, Typography } from '@material-ui/core'
-import React from 'react'
+import { Box, Grid, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+const useStyles = makeStyles((theme) => ({
+  firstcolor: {
+    color: "#80a093",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "30px",
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: "0px",
+    },
+  },
+  secondcolor: {
+    color: "#ead4c0",
+    marginBottom: "30px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: "0px",
+    },
+  },
+  thirdcolor: {
+    color: "#80a093",
+    alignItems: "center",
+    justifyContent: "center",
+
+    [theme.breakpoints.up("sm")]: {
+      justifyContent: "flex-end",
+    },
+  },
+  firstTextStyle: {
+    fontFamily: "Josefin Sans",
+    fontSize: "80%",
+    fontWeight: "bold",
+  },
+  secondTextStyle: {
+    fontFamily: "Josefin Sans",
+    fontWeight: "bold",
+  },
+}));
 
 const QuickContact = () => {
-    return (
-        <Box style={{minWidth: "100%" , backgroundColor: "#024126",}} pt={5}>
+  const classes = useStyles();
+  return (
+    <Box style={{ minWidth: "100%", backgroundColor: "#024126" }} pt={5}>
+      <Grid item container>
+        {/* First Padding For Desktop*/}
+        <Grid item xs={0} sm={3} />
+        {/* Facebook */}
 
-        <Grid container >
-            {/* First Padding */}
-            <Grid item xs={0} sm={3} justify="center"/>
-            {/* Facebook */}
-           <Grid item container xs={12} sm={1} justify="center">
-               <Typography>
-                   facebook
-               </Typography>
-           </Grid>
-           {/* padding between facebook and enjoy */}
-            <Grid item xs={0} sm= {1} />
-            {/* Enjoy passion */}
-           <Grid item container xs={12} sm={2} justify="center">
-               <Typography>
-               Enjoy your passion for wine with us
-               </Typography>
-           </Grid>
-           <Grid item xs={0} sm={1} />
-           <Grid item container xs={12} sm={1} justify="center">
-               <Typography>
-               play video
-               </Typography>
-           </Grid>
-           {/* Last Padding */}
-           <Grid item xs={0} sm={3}/>
+        <Grid item container xs={12} sm={1} className={classes.firstcolor}>
+          <Grid item>
+            <FacebookIcon className={classes.icon} />
+          </Grid>
+          <Grid item>
+            <Typography className={classes.firstTextStyle}>FACEBOOK</Typography>
+          </Grid>
         </Grid>
-        </Box>
-    )
-}
+        {/* padding between facebook and enjoy for Desktop */}
+        <Grid item xs={0} sm={1} />
+        {/* Enjoy passion */}
+        <Grid
+          item
+          container
+          xs={12}
+          sm={2}
+          className={classes.secondcolor}
+          justify="center"
+        >
+          <Typography className={classes.secondTextStyle}>
+            Enjoy your passion for wine with us
+          </Typography>
+        </Grid>
+        {/* padding between enjoy and video */}
+        <Grid item xs={0} sm={1} />
+        {/* Play Video */}
+        <Grid item container xs={12} sm={1} className={classes.thirdcolor}>
+          <Grid item>
+            <Typography className={classes.firstTextStyle}>
+              PLAY VIDEO
+            </Typography>
+          </Grid>
+          <Grid item>
+            <PlayArrowIcon />
+          </Grid>
+        </Grid>
+        {/* Last Padding For Desktop */}
+        <Grid item xs={0} sm={3} />
+      </Grid>
+    </Box>
+  );
+};
 
-export default QuickContact
+export default QuickContact;
