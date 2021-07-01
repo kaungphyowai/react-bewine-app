3845bd22efac85c97252d55795133efbc3258662
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-07-01 19:12:37 +0630

    counting number animatied

--- src/components/NumberContent.js
+++ src/components/NumberContent.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
+import CountUp from "react-countup";
 
 const NumberContent = ({ subtitleStyle, descriptionFaded, numberTitle }) => {
   return (
     <Grid item container direction="column" xs={12} sm={3} spacing={1}>
       <Grid container item xs={12} justify="center">
-        <Typography className={numberTitle}>124</Typography>
+        <Typography className={numberTitle}>
+          <CountUp start={0} end={126} duration={4} />
+        </Typography>
       </Grid>
       <Grid item container justify="center">
         <Typography
