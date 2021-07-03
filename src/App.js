3e76442799eced020fb3067ead252222cf2ab808
Kaung Phyo Wai <kaungphyowaidecember@gmail.com>
2021-07-03 18:34:12 +0630

    routing complete

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import AboutUsPage from "./components/Pages/AboutUsPage";
 import JoinNow from "./components/HomeComponent/JoinNow";
 import Wines from "./components/Pages/Wines";
 import ContactPage from "./components/Pages/ContactPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
-    <>
+    <Router>
       <NavBar></NavBar>
-      <HomePage />
-      <AboutUsPage />
-      <Wines />
-      <ContactPage />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/about">
+          <AboutUsPage />
+        </Route>
+        <Route exact path="/wines">
+          <Wines />
+        </Route>
+        <Route exact path="/contact">
+          <ContactPage />
+        </Route>
+      </Switch>
+
       <JoinNow />
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,47 +1,58 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-const useStyles = makeStyles(theme => ({
-    title: {
-        flexGrow: 1,
-        textAlign: 'center',
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  linkStyle: {
+    color: "#f5e8dc",
+  },
+  paper: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
     },
-    paper: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(1),
-            width: theme.spacing(16),
-            height: theme.spacing(16),
-        },
-
-    },
-    wrap: {
-        display: "block",
-    },
-    offset: theme.mixins.toolbar,
+  },
+  wrap: {
+    display: "block",
+  },
+  offset: theme.mixins.toolbar,
 }));
 
-
 const NavBar = () => {
-    const classes = useStyles();
-    return (
-        <>
-            <AppBar position="fixed">
-            <Toolbar>
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Bewine
+          </Typography>
+          <Link to="/">
+            <Button className={classes.linkStyle}>HOME</Button>
+          </Link>
+          <Link to="/about">
+            <Button className={classes.linkStyle}>ABOUT US</Button>
+          </Link>
+          <Link to="/wines">
+            <Button className={classes.linkStyle}>WINES</Button>
+          </Link>
+          <Link to="/contact">
+            <Button className={classes.linkStyle}>CONTACT</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
+    </>
+  );
+};
 
-                <Typography variant="h6" className={classes.title}>
-                        Bewine
-                    </Typography>
-                    <Button >Login</Button>
-            </Toolbar>
-            </AppBar>
-            <div className={classes.offset} />
-        </>
-    );
-}
-
-export default NavBar
+export default NavBar;
